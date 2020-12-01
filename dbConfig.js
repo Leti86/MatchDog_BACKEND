6bb0409acc9c8405166013e5b3881593f5689cd2
Leti86 <leticia.anchuelo@gmail.com>
2020-12-01 17:10:21 +0100

    leti: dbConfig.js / fichero .env

---
+++ dbConfig.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const createPool = () => {
+    const pool = mysql.createPool({//datos incluidos en fichero .env
+        host: process.env.db_host,
+        user: process.env.db_user,
+        password: process.env.db_password,
+        port: process.env.db_port,
+        database: process.env.db_name
+    });
+    global.db = pool;
+}
+
+module.exports = {
+    createPool
+}
